089ad6ac1fb3f5a5f2159c88d1ec5c8f709541dc
allallisyou <allallisyou@gmail.com>
2025-03-12 23:47:33 +0800

    feat:加入work的一些配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "umi";
+import path from "path";
 
 export default defineConfig({
   routes: [
@@ -6,4 +7,14 @@ export default defineConfig({
     { path: "/docs", component: "docs" },
   ],
   npmClient: 'pnpm',
+  alias: {
+    'vtk.js': path.resolve(__dirname), // 指向 src 同级目录的 Sources
+  },
+  chainWebpack(memo) {
+    memo.module
+        .rule('worker')
+        .test(/\.worker\.js$/)
+        .use('worker-loader')
+        .loader('worker-loader');
+  },
 });
